31d946b7a75d8ec593fb510bf9e0524ef532869a
idaism <ida.bjarney@outlook.com>
2023-02-06 16:46:03 +0000

    finished implementing carousel

--- assets/landing-carousel.js
+++ assets/landing-carousel.js
@@ -1,14 +1,15 @@
 const slideImg = document.querySelector('.slide-img-container');
-const controlButtons = Array.from(document.querySelectorAll('.carousel-control-buttons'));
+const controlButtons = Array.from(document.querySelectorAll('.carousel-control-buttons button'));
 const backBtn = document.querySelector('.back-button');
 const forwardBtn = document.querySelector('.forward-button');
 
 let currentSlide = 0;
 
-backBtn.addEventListener('click, slideForward');
-forwardBtn.addEventListener('click, slideBack');
+backBtn.addEventListener('click', slideBack);
+forwardBtn.addEventListener('click', slideForward);
 
-function slideFoward() {
+function slideForward() {
+  console.log('click')
   if (currentSlide === 3) {
     currentSlide = 0;
   } else {
@@ -17,5 +18,28 @@ function slideFoward() {
   document.querySelector('.active').classList.remove('active');
   controlButtons[currentSlide].classList.add('active');
   moveSlider();
-};
+}
 
+function slideBack() {
+  if (currentSlide === 0) {
+  currentSlide = 3;
+  } else {
+    currentSlide--;
+  }
+  document.querySelector('.active').classList.remove('active');
+  controlButtons[currentSlide].classList.add('active');
+  moveSlider();
+}
+
+controlButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    currentSlide = index;
+    document.querySelector('.active').classList.remove('active');
+    button.classList.add('active')
+    moveSlider();
+  })
+})
+
+function moveSlider() {
+  slideImg.style.transform = `translateX(-${currentSlide * 25}%)`
+}
