fca426740dc6ce21478eb88095cbf9bfe2598561
idaism <ida.bjarney@outlook.com>
2023-02-07 14:01:18 +0000

    Created shrink header function for scroll

--- assets/header.js
+++ assets/header.js
@@ -2,6 +2,9 @@ const hamburgerBtn = document.querySelector('.hamburger-btn');
 const closeBtn = document.querySelector('.close-btn');
 const nav = document.querySelector('nav');
 const navWrapper = document.querySelector('.nav-wrapper');
+const headerDiv = document.querySelector('header');
+
+let shrinkHeader = 115;
 
 hamburgerBtn.addEventListener('click', function() {
   navWrapper.classList.remove('hidden');
@@ -19,3 +22,16 @@ navWrapper.addEventListener('click', function(e) {
  closeBtn.addEventListener('click', function () {
   navWrapper.classList.add('hidden');
  })
+
+ window.addEventListener('scroll', function() {
+  const scroll = currentScrollPosition();
+  if (scroll >= shrinkHeader) {
+    headerDiv.classList.add('smaller');
+  } else {
+    headerDiv.classList.remove('smaller');
+  }
+ });
+
+ function currentScrollPosition() {
+  return window.scrollY
+ };
