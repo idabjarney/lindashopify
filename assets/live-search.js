57b6c66a1375590e69d2da566442cbf7d9f3420e
idaism <ida.bjarney@outlook.com>
2023-02-08 17:01:04 +0000

    added predictive search and set up basic js to link it in the search wrapper

--- assets/live-search.js
+++ assets/live-search.js
@@ -1,8 +1,18 @@
  const liveSearchContainer = document.querySelector('.live-search-container');
  const liveSearchInput = document.querySelector('.live-search-input');
+ const resultContainer = document.querySelector('.result');
 
  liveSearchInput.addEventListener('input', function(e) {
    const inputValue = e.target.value;
-   console.log(`${window.location.origin}/search?q=${inputValue}`);
-   
+   if (inputValue === '') return;
+   const queryString = `${window.location.origin}/search/suggest?q=${inputValue}&section_id=predictive-search`;
+   renderResults(queryString);
  })
+
+ async function renderResults(query) {
+  const response = await fetch(query)
+  if (response.status === 200) {
+    const responseMarkup = await response.text();
+    resultContainer.innerHTML = responseMarkup;
+  }
+ }
