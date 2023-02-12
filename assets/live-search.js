daa1b7a967e59f5684241d85a533e332adbb1e83
idaism <ida.bjarney@outlook.com>
2023-02-12 11:59:30 +0000

    styled live search and edited js to only show 5 results on the page

--- assets/live-search.js
+++ assets/live-search.js
@@ -1,18 +1,36 @@
  const liveSearchContainer = document.querySelector('.live-search-container');
  const liveSearchInput = document.querySelector('.live-search-input');
- const resultContainer = document.querySelector('.result');
+ const resultContainer = document.querySelector('.search-result ul');
 
  liveSearchInput.addEventListener('input', function(e) {
    const inputValue = e.target.value;
-   if (inputValue === '') return;
+   if (inputValue === '') {
+    resultContainer.innerHTML = '';
+    return;
+   } 
    const queryString = `${window.location.origin}/search/suggest?q=${inputValue}&section_id=predictive-search`;
-   renderResults(queryString);
+   renderResults(queryString, e.target);
  })
 
- async function renderResults(query) {
+ async function renderResults(query, inputElement) {
   const response = await fetch(query)
   if (response.status === 200) {
+    resultContainer.innerHTML = '';
     const responseMarkup = await response.text();
-    resultContainer.innerHTML = responseMarkup;
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(responseMarkup, "text/html");
+    const products = Array.from(doc.querySelectorAll('.li-product-card'));
+    if (inputElement.value === '') return;
+    if (products.length >= 5) {
+      products.splice(0, 5).forEach(product => {
+        resultContainer.appendChild(product);
+        const anchor = document.createElement('a');
+        // anchor.href = 
+      });
+    } else {
+      products.forEach(product => {
+        resultContainer.appendChild(product);
+      });
+    }
   }
  }
