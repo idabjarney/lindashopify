e923aa89a5bd009c720cd9acd8dd8684682a74ed
idaism <ida.bjarney@outlook.com>
2023-02-15 11:57:27 +0000

    added paragraphs for products and no results to live search. Finished styling and functionality for search drawer

--- assets/live-search.js
+++ assets/live-search.js
@@ -21,25 +21,33 @@
     const parser = new DOMParser();
     const doc = parser.parseFromString(responseMarkup, "text/html");
     const products = Array.from(doc.querySelectorAll('.li-product-card'));
+    const producth3 = document.createElement('h3');
+    producth3.innerText = 'Products';
+    producth3.className = 'product-h3';
     if (inputElement.value === '') return;
     if (products.length > 5) {
+      resultContainer.appendChild(producth3);
       products.splice(0, 5).forEach(product => {
         resultContainer.appendChild(product);
       });
+      
       const inputValue = inputElement.value;
       const anchor = document.createElement('a');
       anchor.href = `/search/?q=${inputValue}`;
-      anchor.innerText = 'Show more';
+      anchor.innerText = 'Show all results';
       resultContainer.appendChild(anchor);
       anchor.className = 'show-more-btn';
-    } else {
-      // const producth2 = document.createElement('h2');
-      // producth2.innerText = 'Products';
-      // resultContainer.appendChild(producth2);
-      // producth2.className = 'product-h2';
+      
+    } else if ( products.length < 5 && products.length > 0) {
+      resultContainer.appendChild(producth3);
       products.forEach(product => {
         resultContainer.appendChild(product);
       });
+    } else {
+      const noProductsFound = document.createElement('h3');
+      noProductsFound.innerText = 'Sorry! No products were found';
+      resultContainer.appendChild(noProductsFound);
+      noProductsFound.className = 'no-products-found-h3'
     }
   }
  }
